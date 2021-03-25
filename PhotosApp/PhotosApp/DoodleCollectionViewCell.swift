0f17498c8891f5c397130ed8c67e30cf1069642a
zeke <yangsin1031@naver.com>
2021-03-25 15:48:01 +0900

    DoodleCollectionViewCell 생성
    DoodleCollectionViewCell 생성

---
+++ PhotosApp/PhotosApp/DoodleCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  DoodleCollectionViewCell.swift
+//  PhotosApp
+//
+//  Created by 양준혁 on 2021/03/25.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    let image: UIImageView
+
+    
+    override init(frame: CGRect) {
+        image = UIImageView()
+        super.init(frame: frame)
+        autolayout()
+        self.reuseIdentifier = "doodleCell"
+    }
+    
+    required init?(coder: NSCoder) {
+        image = UIImageView()
+        super.init(coder: coder)
+        autolayout()
+        self.reuseIdentifier = "doodleCell"
+    }
+    
+    func autolayout() {
+        image.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        image.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        image.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        image.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        self.addSubview(image)
+    }
+}
